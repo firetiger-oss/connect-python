ad80de36203a7403fc943b3c2345574fdbd55c89
Spencer Nelson <s@spencerwnelson.com>
2025-06-18 00:32:02 -0700

    Regenerate example

--- examples/eliza_pb2_connect.py
+++ examples/eliza_pb2_connect.py
@@ -1,20 +1,17 @@
 # Generated Connect client code
 
 from collections.abc import AsyncIterator
-
 import aiohttp
-from eliza_pb2 import ConverseRequest
-from eliza_pb2 import ConverseResponse
-from eliza_pb2 import IntroduceRequest
-from eliza_pb2 import IntroduceResponse
-from eliza_pb2 import SayRequest
-from eliza_pb2 import SayResponse
 
 from connectrpc.client import ConnectClient
 from connectrpc.client import ConnectProtocol
+from connectrpc.client_connect import ConnectProtocolError
+from connectrpc.headers import HeaderInput
 from connectrpc.streams import StreamInput
 from connectrpc.streams import StreamOutput
+from connectrpc.unary import UnaryOutput
 
+import eliza_pb2
 
 class ElizaServiceClient:
     def __init__(
@@ -26,34 +23,72 @@ class ElizaServiceClient:
         self.base_url = base_url
         self._connect_client = ConnectClient(http_client, protocol)
 
-    async def say(self, req: SayRequest) -> SayResponse:
+    async def call_say(
+        self, req: eliza_pb2.SayRequest,extra_headers: HeaderInput | None=None, timeout_seconds: float | None=None
+    ) -> UnaryOutput[eliza_pb2.SayResponse]:
+        """Low-level method to call Say, granting access to errors and metadata"""
         url = self.base_url + "/connectrpc.eliza.v1.ElizaService/Say"
-        return await self._connect_client.call_unary(url, req, SayResponse)
+        return await self._connect_client.call_unary(url, req, eliza_pb2.SayResponse,extra_headers, timeout_seconds)
 
-    def converse(self, reqs: StreamInput[ConverseRequest]) -> AsyncIterator[ConverseResponse]:
-        return self._converse_impl(reqs)
+    async def say(
+        self, req: eliza_pb2.SayRequest,extra_headers: HeaderInput | None=None, timeout_seconds: float | None=None
+    ) -> eliza_pb2.SayResponse:
+        response = await self.call_say(req, extra_headers, timeout_seconds)
+        err = response.error()
+        if err is not None:
+            raise err
+        msg = response.message()
+        if msg is None:
+            raise ConnectProtocolError('missing response message')
+        return msg
 
-    async def _converse_impl(
-        self, reqs: StreamInput[ConverseRequest]
-    ) -> AsyncIterator[ConverseResponse]:
-        async with await self.converse_stream(reqs) as stream:
+    def converse(
+        self, reqs: StreamInput[eliza_pb2.ConverseRequest], extra_headers: HeaderInput | None=None, timeout_seconds: float | None=None
+    ) -> AsyncIterator[eliza_pb2.ConverseResponse]:
+        return self._converse_iterator(reqs, extra_headers, timeout_seconds)
+
+    async def _converse_iterator(
+        self, reqs: StreamInput[eliza_pb2.ConverseRequest], extra_headers: HeaderInput | None=None, timeout_seconds: float | None=None
+    ) -> AsyncIterator[eliza_pb2.ConverseResponse]:
+        stream_output = await self.call_converse(reqs, extra_headers, timeout_seconds)
+        err = stream_output.error()
+        if err is not None:
+            raise err
+        async with stream_output as stream:
             async for response in stream:
                 yield response
 
-    async def converse_stream(
-        self, reqs: StreamInput[ConverseRequest]
-    ) -> StreamOutput[ConverseResponse]:
+    async def call_converse(
+        self, reqs: StreamInput[eliza_pb2.ConverseRequest], extra_headers: HeaderInput | None=None, timeout_seconds: float | None=None
+    ) -> StreamOutput[eliza_pb2.ConverseResponse]:
+        """Low-level method to call Converse, granting access to errors and metadata"""
         url = self.base_url + "/connectrpc.eliza.v1.ElizaService/Converse"
-        return await self._connect_client.call_bidirectional_streaming(url, reqs, ConverseResponse)
+        return await self._connect_client.call_bidirectional_streaming(
+            url, reqs, eliza_pb2.ConverseResponse, extra_headers, timeout_seconds
+        )
 
-    def introduce(self, req: IntroduceRequest) -> AsyncIterator[IntroduceResponse]:
-        return self._introduce_impl(req)
+    def introduce(
+        self, req: eliza_pb2.IntroduceRequest,extra_headers: HeaderInput | None=None, timeout_seconds: float | None=None
+    ) -> AsyncIterator[eliza_pb2.IntroduceResponse]:
+        return self._introduce_iterator(req, extra_headers, timeout_seconds)
 
-    async def _introduce_impl(self, req: IntroduceRequest) -> AsyncIterator[IntroduceResponse]:
-        async with await self.introduce_stream(req) as stream:
+    async def _introduce_iterator(
+        self, req: eliza_pb2.IntroduceRequest,extra_headers: HeaderInput | None=None, timeout_seconds: float | None=None
+    ) -> AsyncIterator[eliza_pb2.IntroduceResponse]:
+        stream_output = await self.call_introduce(req, extra_headers)
+        err = stream_output.error()
+        if err is not None:
+            raise err
+        async with stream_output as stream:
             async for response in stream:
                 yield response
 
-    async def introduce_stream(self, req: IntroduceRequest) -> StreamOutput[IntroduceResponse]:
+    async def call_introduce(
+        self, req: eliza_pb2.IntroduceRequest,extra_headers: HeaderInput | None=None, timeout_seconds: float | None=None
+    ) -> StreamOutput[eliza_pb2.IntroduceResponse]:
+        """Low-level method to call Introduce, granting access to errors and metadata"""
         url = self.base_url + "/connectrpc.eliza.v1.ElizaService/Introduce"
-        return await self._connect_client.call_server_streaming(url, req, IntroduceResponse)
+        return await self._connect_client.call_server_streaming(
+            url, req, eliza_pb2.IntroduceResponse, extra_headers, timeout_seconds
+        )
+
