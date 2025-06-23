ab54da9c4ec3810c21bd6e1539f33596f73c44c4
Spencer Nelson <spencer@firetiger.com>
2025-06-23 09:27:25 -0700

    Regenerate eliza with service

--- examples/eliza_pb2.pyi
+++ examples/eliza_pb2.pyi
@@ -1,7 +1,6 @@
-from typing import ClassVar as _ClassVar
-
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -9,34 +8,34 @@ class SayRequest(_message.Message):
     __slots__ = ("sentence",)
     SENTENCE_FIELD_NUMBER: _ClassVar[int]
     sentence: str
-    def __init__(self, sentence: str | None = ...) -> None: ...
+    def __init__(self, sentence: _Optional[str] = ...) -> None: ...
 
 class SayResponse(_message.Message):
     __slots__ = ("sentence",)
     SENTENCE_FIELD_NUMBER: _ClassVar[int]
     sentence: str
-    def __init__(self, sentence: str | None = ...) -> None: ...
+    def __init__(self, sentence: _Optional[str] = ...) -> None: ...
 
 class ConverseRequest(_message.Message):
     __slots__ = ("sentence",)
     SENTENCE_FIELD_NUMBER: _ClassVar[int]
     sentence: str
-    def __init__(self, sentence: str | None = ...) -> None: ...
+    def __init__(self, sentence: _Optional[str] = ...) -> None: ...
 
 class ConverseResponse(_message.Message):
     __slots__ = ("sentence",)
     SENTENCE_FIELD_NUMBER: _ClassVar[int]
     sentence: str
-    def __init__(self, sentence: str | None = ...) -> None: ...
+    def __init__(self, sentence: _Optional[str] = ...) -> None: ...
 
 class IntroduceRequest(_message.Message):
     __slots__ = ("name",)
     NAME_FIELD_NUMBER: _ClassVar[int]
     name: str
-    def __init__(self, name: str | None = ...) -> None: ...
+    def __init__(self, name: _Optional[str] = ...) -> None: ...
 
 class IntroduceResponse(_message.Message):
     __slots__ = ("sentence",)
     SENTENCE_FIELD_NUMBER: _ClassVar[int]
     sentence: str
-    def __init__(self, sentence: str | None = ...) -> None: ...
+    def __init__(self, sentence: _Optional[str] = ...) -> None: ...
